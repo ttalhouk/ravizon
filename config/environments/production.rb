02c89e99358e1e89c95bf37218fea9a9e81dc333
Talal Talhouk <talal@sbcglobal.net>
2017-10-15 22:29:06 -0700

    config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,6 @@ Rails.application.configure do
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     },
-    :s3_host_name => "s3-#{ENV.fetch('AWS_REGION')}.amazonaws.com", # Added entry
+    # :s3_host_name => "s3-#{ENV.fetch('AWS_REGION')}.amazonaws.com", # Added entry
   }
 end
