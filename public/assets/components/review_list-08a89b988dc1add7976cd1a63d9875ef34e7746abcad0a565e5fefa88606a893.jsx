c5a1934b8131a8b98c2ebd4f08ed7d95c045529a
Talal Talhouk <talal@sbcglobal.net>
2017-10-16 13:28:22 -0700

    recomple javascript assetts

---
+++ public/assets/components/review_list-08a89b988dc1add7976cd1a63d9875ef34e7746abcad0a565e5fefa88606a893.jsx
@@ -0,0 +1,102 @@
+class ReviewList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openForm:false,
+      reviews: []
+    }
+  }
+  componentWillMount(){
+    this.setState({reviews: this.props.reviews})
+
+  }
+
+  handleFormSubmit(reviewData) {
+    console.log(reviewData);
+
+    // ajax request to review create route
+    $.ajax({
+      url: `/products/${this.props.product.id}/reviews`,
+      method: 'POST',
+      dataType: 'json',
+      data: {review: reviewData},
+      context: this
+    })
+      .success((data) => {
+        // this.props.updateMessage({success: `review successfully added`})
+        console.log(data);
+        review = data.review;
+        review['reviewer'] = this.props.user.first_name
+        this.setState({reviews: [review].concat(this.state.reviews)})
+        this.handleOpenForm();
+      })
+      .error((err) => {
+        if (err.responseJSON.redirect){
+          window.location.replace(err.responseJSON.redirect)
+        } else {
+          console.log(err.responseJSON);
+        }
+      })
+  }
+
+  renderReviews() {
+    return this.state.reviews.map((review) => {
+      return(
+        <Review key={review.id} review={review} reviewer={review.reviewer} />
+      );
+    });
+  }
+
+  renderReviewForm() {
+    return (
+      <ReviewForm
+        handleSubmit={this.handleFormSubmit.bind(this)}
+        openForm={this.state.openForm}
+        />
+    )
+  }
+
+  handleOpenForm(){
+    this.setState({openForm: !this.state.openForm});
+  }
+
+  renderReviewButton(){
+    if (this.props.user) {
+      return (
+        <button
+          onClick={this.handleOpenForm.bind(this)}
+          className="button button--action">
+          {this.state.openForm ? "Close" : "Leave a Review"}
+        </button>
+      );
+    } else {
+      return;
+    }
+  }
+
+  render () {
+    return (
+      <div className='page--review-section'>
+        <div className='review'>
+          {this.renderReviewButton()}
+
+
+          {this.renderReviewForm()}
+          <FlipMove
+            className='review-list'
+            duration={500}
+            easing="ease-out"
+            >
+            {this.renderReviews()}
+          </FlipMove>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReviewList.propTypes = {
+  product: React.PropTypes.object,
+  user: React.PropTypes.object,
+  reviews: React.PropTypes.array
+};
